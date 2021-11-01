dc304283ac39f2e8a5d50b858d0d1f11b0682f0a
Basila123 <basilanoushad92@gmail.com>
2021-11-01 23:18:46 +0530

    Added how to append the value

---
+++ MainLinkedList.java
@@ -0,0 +1,16 @@
+package com.bridgelabzAddress;
+import java.util.*;
+public class MainLinkedList {
+    public static void main(String[] arg){
+        LinkedList n=new LinkedList();
+        Scanner s= new Scanner(System.in);
+        for(int i =0; i<4;i++){
+            System.out.println("Enter a number");
+            int d=s.nextInt();
+            n.push(d);
+        }
+        System.out.println("The pushed data are:");
+        n.traverse();
+        }
+}
+
---
+++ Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabzAddress;
+
+public class Node {
+    int data;
+    Node next=null;
+    Node(int data, Node next){
+        this.data=data;
+        this.next=null;
+
+    }
+}
